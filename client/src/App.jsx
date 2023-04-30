fc990ce1aabae52226b93a28a24ea1742ee059f2
Flexxbo <flexxbo@gmail.com>
2023-04-30 19:15:18 +0200

    feat: Add new imports and form input with state hook in App.jsx
    Import styles from ./index.module.css and sqlLogo from ./assets/sql-server.png for styling and image display purposes. Also import useState hook from React for managing state in the component.
    
    Add a form input with a state hook to manage the query description entered by the user. When the input value changes, set the state with the new value using the setQueryDescription function.
    
    The form contains a single input field for query description and a submit button to generate the query.
    
    This update enhances the user interface and sets up the functionality for generating SQL queries with AI.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import styles from "./index.module.css"
+import sqlLogo from "./assets/sql-server.png"
+import { useState } from "react"
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queryDescription, setQueryDescription] = useState("")
 
   return (
-  <h1>hello world</h1>
-  )
+    <main className={styles.main}>
+     <img src={sqlLogo} alt="" className={styles.icon} /> 
+     <h3>Generate SQL with AI</h3>
+     <form>
+      <input type="text"
+      name="query-description"
+      placeholder="Describe your query"
+      onChange={()=> setQueryDescription(e.target.value)} />
+      <input type="submit" value="Generate Query" />
+
+
+
+     </form>
+
+
+
+    </main>  )
 }
 
 export default App

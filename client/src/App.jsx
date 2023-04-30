f2927ce23babe67c634ec48fb0e0bec577ec204d
Flexxbo <flexxbo@gmail.com>
2023-04-30 19:23:45 +0200

    refactor: Add form submit handler and update onSubmit function in App.jsx
    In this commit, I added a form submit handler function named onSubmit to prevent the default form submission behavior and log a message to the console. The onSubmit function is attached to the form as an event handler.
    
    I also updated the onChange function of the input field to use the event object (e) and set the queryDescription state based on the value entered by the user.
    
    This commit improves the functionality of the form and lays the groundwork for generating SQL queries with AI in the future.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,30 @@
-import styles from "./index.module.css"
-import sqlLogo from "./assets/sql-server.png"
-import { useState } from "react"
-
-
+import styles from "./index.module.css";
+import sqlLogo from "./assets/sql-server.png";
+import { useState } from "react";
 
 function App() {
-  const [queryDescription, setQueryDescription] = useState("")
+  const [queryDescription, setQueryDescription] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
 
   return (
     <main className={styles.main}>
-     <img src={sqlLogo} alt="" className={styles.icon} /> 
-     <h3>Generate SQL with AI</h3>
-     <form>
-      <input type="text"
-      name="query-description"
-      placeholder="Describe your query"
-      onChange={()=> setQueryDescription(e.target.value)} />
-      <input type="submit" value="Generate Query" />
-
-
-
-     </form>
-
-
-
-    </main>  )
+      <img src={sqlLogo} alt="" className={styles.icon} />
+      <h3>Generate SQL with AI</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query"
+          onChange={(e) => setQueryDescription(e.target.value)}
+        />
+        <input type="submit" value="Generate Query" />
+      </form>
+    </main>
+  );
 }
 
-export default App
+export default App;

a640e3a1f5b4f8129f7b375313de0d09a476fd40
Flexxbo <flexxbo@gmail.com>
2023-05-02 20:04:01 +0200

    Added a POST endpoint to index.js /generate,,,,This endpoint is used to receive a query description from the client and log it to the console. A response is sent back to the client indicating the received query description.

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ import cors from "cors";
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 3005;
 
 app.get("/", (req, res) => {
   res.send("Hello World from our API!");
+});
+
+app.post("/generate", async (req, res) => {
+    const queryDescription = req.body.queryDescription;
+    console.log("received description: ",queryDescription);
+        res.json({response: `you sent this: ${queryDescription}`})
 });
 
 app.listen(port, () => {
